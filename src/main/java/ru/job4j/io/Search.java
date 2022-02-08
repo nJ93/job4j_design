9d9e12e9467dde50d5c3342b6bf37fe3d04a9615
nJ93 <vit-chembaev@yandex.ru>
2022-02-08 22:18:21 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,20 @@ import java.util.function.Predicate;
 
 public class Search {
   public static void main(String[] args) throws IOException {
-    Path start = Paths.get(".");
-    search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    if (args.length == 0) {
+      throw new IllegalArgumentException("Empty input arguments");
+    }
+    String dirArgument = args[0];
+    String extensionArgument = args[1];
+    if (dirArgument == null || extensionArgument == null) {
+      throw new IllegalArgumentException("Missing one of argument. Need to check input settings.");
+    }
+    File file = new File(dirArgument);
+    if (!file.isDirectory()) {
+      throw new IllegalArgumentException("First argument is not a directory");
+    }
+
+    search(file.toPath(), p -> p.toFile().getName().endsWith(extensionArgument)).forEach(System.out::println);
   }
 
   public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
