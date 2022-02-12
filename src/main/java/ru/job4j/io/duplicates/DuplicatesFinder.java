03d75a74db3f7b952f7c38f908404c8c4a181efe
nJ93 <vit-chembaev@yandex.ru>
2022-02-12 21:17:37 +0300

    Поиск дубликатов [#315066 #262188] [fix algorithm]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,13 +3,14 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Set;
 
 public class DuplicatesFinder {
   public static void main(String[] args) throws IOException {
     DuplicatesVisitor visitor = new DuplicatesVisitor();
     Files.walkFileTree(Path.of(""), visitor);
-    Set<FileProperty> duplicateFiles = visitor.getDuplicateFiles();
+    List<FileProperty> duplicateFiles = visitor.getDuplicateFiles();
     duplicateFiles.forEach(System.out::println);
   }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -4,23 +4,26 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-  private final Set<FileProperty> alreadyExistsFiles = new HashSet<>();
-  private final Set<FileProperty> duplicateFiles = new HashSet<>();
+  private final List<FileProperty> alreadyExistsFiles = new ArrayList<>();
 
   @Override
   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
     FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString(), file.toAbsolutePath().toString());
-    if (!alreadyExistsFiles.add(fileProperty)) {
-      duplicateFiles.add(fileProperty);
-    }
+    alreadyExistsFiles.add(fileProperty);
     return FileVisitResult.CONTINUE;
   }
 
-  public Set<FileProperty> getDuplicateFiles() {
-    return duplicateFiles;
+  public List<FileProperty> getDuplicateFiles() {
+    return alreadyExistsFiles
+            .stream()
+            .filter(elem -> alreadyExistsFiles.indexOf(elem) != alreadyExistsFiles.lastIndexOf(elem))
+            .collect(Collectors.toList());
   }
 }
