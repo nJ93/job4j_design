5da040ce1d3fb2184aa5197b7703c5622a0eedab
nJ93 <vit-chembaev@yandex.ru>
2022-02-23 21:51:01 +0300

    Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,63 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+  private Path mainSource;
+
+  public void packFiles(List<Path> sources, File target) {
+    try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+      for (Path source : sources) {
+        Path sourceFile = mainSource.relativize(source);
+        zip.putNextEntry(new ZipEntry(sourceFile.toString()));
+        try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
+          zip.write(out.readAllBytes());
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void packSingleFile(File source, File target) {
+    try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+      zip.putNextEntry(new ZipEntry(source.getPath()));
+      try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+        zip.write(out.readAllBytes());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private boolean validateInputArguments(ArgsName args) {
+    File source = new File(args.get("d"));
+    args.get("e");
+    args.get("o");
+    if (!source.isDirectory()) {
+      throw new IllegalArgumentException(source + " is not a directory");
+    }
+    return true;
+  }
+
+  public void setMainSource(Path source) {
+    this.mainSource = source;
+  }
+
+  public static void main(String[] args) throws IOException {
+    Zip zip = new Zip();
+    ArgsName argsName = ArgsName.of(args);
+    if (zip.validateInputArguments(argsName)) {
+      String directory = argsName.get("d");
+      String extensionToExclude = argsName.get("e");
+      String output = argsName.get("o");
+      zip.setMainSource(Path.of(directory));
+      List<Path> allFiles = Search.search(Path.of(directory), p -> !p.toFile().getName().endsWith(extensionToExclude));
+      zip.packFiles(allFiles, new File(output));
+    }
+  }
+}
