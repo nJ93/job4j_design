c735790ae7c87814f9b98effa4841430be039683
nJ93 <vit-chembaev@yandex.ru>
2022-04-23 22:25:27 +0300

    Связи между таблицами [#373615] fix#1 onetoone

--- scripts/onetoone.sql
+++ scripts/onetoone.sql
@@ -1,11 +1,11 @@
-CREATE TABLE registration_address (
+CREATE TABLE driver_license (
 	id serial primary key,
-	address text
+	serial_number text
 );
 
 CREATE TABLE person (
 	id serial primary key,
 	name text,
 	age int,
-	registration_address_id int references registration_address(id) unique
+	driver_license_id int references driver_license(id) unique
 );
