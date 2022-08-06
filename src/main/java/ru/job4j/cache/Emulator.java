d21b4bc9c3b38ea39be618111271ff6b17d2b65b
nJ93 <vit-chembaev@yandex.ru>
2022-08-06 16:41:22 +0300

    Реализация кеша на SoftReference [#1592] fix line separators

--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -11,11 +11,11 @@ public class Emulator {
   private static final int SET_DIRECTORY = 1;
   private static final int LOAD_FILE = 2;
   private static final int GET_FILE = 3;
-  private static final String WRONG_ENTER_MSG = "Wrong enter\n";
-  private static final String SET_DIRECTORY_TO_WORK_MSG = "Set directory to work\n";
-  private static final String ENTER_FILE_NAME_MSG = "Enter file name\n";
-  private static final String TYPE_PATH_TO_DIR_MSG = "Type path to directory\n";
-  private static final String FILE_LOADED_IN_CACHE_MSG = "File loaded in cache\n";
+  private static final String WRONG_ENTER_MSG = "Wrong enter%n";
+  private static final String SET_DIRECTORY_TO_WORK_MSG = "Set directory to work%n";
+  private static final String ENTER_FILE_NAME_MSG = "Enter file name%n";
+  private static final String TYPE_PATH_TO_DIR_MSG = "Type path to directory%n";
+  private static final String FILE_LOADED_IN_CACHE_MSG = "File loaded in cache%n";
   private static final String SHOW_ENTER_PHRASES = """
           1. Set directory
           2. Load file in cache
@@ -26,7 +26,7 @@ public class Emulator {
   public static void main(String[] args) {
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
       AbstractCache<String, String> cache = null;
-      System.out.println(SHOW_ENTER_PHRASES);
+      System.out.printf(SHOW_ENTER_PHRASES);
       String enter = "";
       String result;
       while (!enter.equals("Exit")) {
@@ -35,50 +35,50 @@ public class Emulator {
         try {
           numberEnter = Integer.parseInt(enter);
         } catch (NumberFormatException e) {
-          System.out.println(WRONG_ENTER_MSG);
+          System.out.printf(WRONG_ENTER_MSG);
           continue;
         }
         switch (numberEnter) {
           case SET_DIRECTORY:
-            System.out.println(TYPE_PATH_TO_DIR_MSG);
+            System.out.printf(TYPE_PATH_TO_DIR_MSG);
             enter = reader.readLine();
             if (!checkPathIsDirectory(enter)) {
-              System.out.println(SHOW_ENTER_PHRASES);
+              System.out.printf(SHOW_ENTER_PHRASES);
               continue;
             }
             cache = new DirFileCache(enter);
-            System.out.println(SHOW_ENTER_PHRASES);
+            System.out.printf(SHOW_ENTER_PHRASES);
             continue;
           case LOAD_FILE:
             if (cache == null) {
-              System.out.println(SET_DIRECTORY_TO_WORK_MSG);
-              System.out.println(SHOW_ENTER_PHRASES);
+              System.out.printf(SET_DIRECTORY_TO_WORK_MSG);
+              System.out.printf(SHOW_ENTER_PHRASES);
               continue;
             }
-            System.out.println(ENTER_FILE_NAME_MSG);
+            System.out.printf(ENTER_FILE_NAME_MSG);
             enter = reader.readLine();
             result = cache.get(enter);
             if (result != null) {
-              System.out.println(FILE_LOADED_IN_CACHE_MSG);
+              System.out.printf(FILE_LOADED_IN_CACHE_MSG);
             }
-            System.out.println(SHOW_ENTER_PHRASES);
+            System.out.printf(SHOW_ENTER_PHRASES);
             continue;
           case GET_FILE:
             if (cache == null) {
-              System.out.println(SET_DIRECTORY_TO_WORK_MSG);
-              System.out.println(SHOW_ENTER_PHRASES);
+              System.out.printf(SET_DIRECTORY_TO_WORK_MSG);
+              System.out.printf(SHOW_ENTER_PHRASES);
               continue;
             }
-            System.out.println(ENTER_FILE_NAME_MSG);
+            System.out.printf(ENTER_FILE_NAME_MSG);
             enter = reader.readLine();
             result = cache.get(enter);
             if (result != null) {
               System.out.println(result);
             }
-            System.out.println(SHOW_ENTER_PHRASES);
+            System.out.printf(SHOW_ENTER_PHRASES);
             continue;
           default:
-            System.out.println(WRONG_ENTER_MSG);
+            System.out.printf(WRONG_ENTER_MSG);
         }
       }
     } catch (IOException e) {
@@ -90,7 +90,7 @@ public class Emulator {
     boolean result = true;
     if (!Files.isDirectory(Path.of(path))) {
       result = false;
-      System.out.println("Wrong path to directory\n");
+      System.out.printf("Wrong path to directory\n");
     }
     return result;
   }
